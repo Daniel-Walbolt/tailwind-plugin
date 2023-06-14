1cec892af7ed348b72ca06bd4ba52df84aa621b0
Daniel Walbolt <61804729+Daniel-Walbolt@users.noreply.github.com>
2023-06-13 22:27:21 -0400

    Delete test-folder directory

--- test-folder/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-import type { App } from 'vue';
-import FullPageLayout from './layout/FullPageLayout.vue';
-
-export function loadAppVueComponents(app: App<any>)
-{
-    app.component('FullPageLayout', FullPageLayout);
-}
