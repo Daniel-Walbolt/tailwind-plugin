71854023cb1ed3be93f0ac57c6b23ba3cb333b73
Daniel Walbolt <dwalbolt@gmail.com>
2023-06-15 23:33:34 -0400

    Improved pre-configured parse function

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import cssParser from './cssParser';
 export * from './types';
 
-function ParseCSSDirectoryPlugin(directoryPath) {
-	return (addUtilities, addComponents, e) => {
+/**
+ * Provides quick and easy usage of the cssParser provided by the tailwind-layer-parser plugin. Call this method in TailwindCSS's plugin()
+ *
+ * Uses the default configuration.
+ */
+function ParseCSSDirectoryPlugin(directoryPath?: string) {
+	return (addUtilities, addComponents) => {
 		const classes = cssParser({ directory: directoryPath });
 		for (let utility of classes.utilities) {
-			addUtilities(e(utility));
+			addUtilities(utility);
 		}
 
 		for (let component of classes.components) {
-			addComponents(e(component));
+			addComponents(component);
 		}
 	};
 }
